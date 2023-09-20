8a599f64b62b2c059265b3aaa2d9c1bfd2fcafbd
BoiledPNutEnjoyer <jbecke3@clemson.edu>
2023-09-20 18:01:48 -0400

    Update BoardPosition.java
    Added remaining contracts

--- cpsc2150/extendedConnectX/models/BoardPosition.java
+++ cpsc2150/extendedConnectX/models/BoardPosition.java
@@ -11,7 +11,7 @@ public class BoardPosition
 {    /**
     * This class is for indicating a specific position on the game board, the class tracks the row and column of the position
     * 
-    * @invariant 0 <= Row<= number of rows on the gameboard AND 0 <= Column <= number of columns on the gameboard
+    * @invariant 0 <= Row < number of rows on the gameboard AND 0 <= Column < number of columns on the gameboard
     */
     private int Row;
     private int Column;
@@ -19,7 +19,7 @@ public class BoardPosition
     /**
      * constructor for the BoardPosition object. sets the instance vars to the values passed in via params
      * 
-     * @pre 0 <= aRow<= number of rows on the gameboard AND 0 <= aColumn <= number of columns on the gameboard
+     * @pre 0 <= aRow < number of rows on the gameboard AND 0 <= aColumn < number of columns on the gameboard
      * 
      * @post Row = aRow AND Column = aColumn
      * 
@@ -32,22 +32,60 @@ public class BoardPosition
         //parameterized constructor for BoardPosition
     }
 
+    /**
+     * Returns the value of the board position stored within the Row field
+     * 
+     * @return Row for this instance of BoardPosition
+     * 
+     * @pre None
+     * 
+     * @post getRow = Row AND Column = #Column
+     */
     public int getRow()
     {
         //returns the row
     }
 
+      /**
+     * Returns the value of the board position stored within the Column field
+     * 
+     * @return Column for this instance of BoardPosition
+     * 
+     * @pre None
+     * 
+     * @post getColumn = Column AND Row = #Row
+     */
     public int getColumn()
     {
         //returns the column
     }
 
+    /**
+     * Returns a boolean value checking if the object stored at the position indicated by Row and Column is equal to parameter Object
+     * 
+     * @return boolean (is object stored at position equal to Object)
+     * 
+     * @param Object the object we are checking to see matches the object indicated by Row and Column
+     * 
+     * @pre none
+     * 
+     * @post equals = boolean (is object stored at position equal to Object) And Row = #Row AND Column = #Column
+     */
     @Override
     public boolean equals(Object obj)
     {
 
     }
-
+    
+    /**
+     * Returns a string representing the object stored at the position indicated by the Row and Column
+     * 
+     * @return string representing the object stored at the position indicated by the Row and Column
+     * 
+     * @pre none
+     * 
+     * @post toString = string representing the object stored at the position indicated by the Row and Column AND Row = #Row AND Column = #Column
+     */
     @Override
     public String toString()
     {
