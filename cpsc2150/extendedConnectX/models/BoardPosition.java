a39c78608e2ebcba4a3be0fb8c6c51d881085c6b
BoiledPNutEnjoyer <jbecke3@clemson.edu>
2023-09-20 11:51:00 -0400

    Update BoardPosition.java
    added documentation for class object and constructor

--- cpsc2150/extendedConnectX/models/BoardPosition.java
+++ cpsc2150/extendedConnectX/models/BoardPosition.java
@@ -8,10 +8,25 @@ Joseph Becker - BoiledPNutEnjoyer
  */
 
 public class BoardPosition
-{
+{    /**
+    * This class is for indicating a specific position on the game board, the class tracks the row and column of the position
+    * 
+    * @invariant 0 <= Row<= number of rows on the gameboard AND 0 <= Column <= number of columns on the gameboard
+    */
     private int Row;
     private int Column;
 
+    /**
+     * constructor for the BoardPosition object. sets the instance vars to the values passed in via params
+     * 
+     * @pre 0 <= aRow<= number of rows on the gameboard AND 0 <= aColumn <= number of columns on the gameboard
+     * 
+     * @post Row = aRow AND Column = aColumn
+     * 
+     * 
+     * @param aRow value to be set as the row position
+     * @param aColumn value to be set as the column position
+     */
     public BoardPosition(int aRow, int aColumn)
     {
         //parameterized constructor for BoardPosition
